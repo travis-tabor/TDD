b4a05aa1db96803b6bcef94bc66d0ab25c24aa0a
Travis Tabor <123790499+travis-tabor@users.noreply.github.com>
2023-02-20 09:52:08 -0600

    Q1 test1

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using TDD
 using Test
 
+graph = [
+          [2, 3],     # list of nodes that can be reached from node 1
+          [1],        # list of nodes that can be reached from node 2
+          [1],        # ", node 3
+          [5],        # ", node 4
+          [4]]        # ", node 5
+
 @testset "TDD.jl" begin
+    @test direct_connect(graph,1) == [1,2,3]
     # Write your tests here.
 end
