fabf70bf7dcc98fce56d376238fd36e37df01427
Travis Tabor <123790499+travis-tabor@users.noreply.github.com>
2023-02-20 23:28:16 -0600

    Is empty implementation

--- src/interval.jl
+++ src/interval.jl
@@ -12,3 +12,15 @@ end
 function Base.maximum(iv::Interval)
     return iv.high
 end
+
+function Base.in(x::Real, iv::Interval)
+    return iv.low <= x <= iv.high
+end
+
+function Base.isempty(iv::Interval)
+    if iv.low >= iv.high
+        return true
+    else
+        return false
+    end
+end
--- test/interval_test.jl
+++ test/interval_test.jl
@@ -2,10 +2,12 @@ using TDD
 using Test
 
 a = Interval(1,5)
+b = Interval()
 
 @testset "interval.jl" begin
     @test typeof(a) == Interval
     @test minimum(a) == 1
     @test maximum(a) == 5
     @test in(2.5, a)
+    @test isempty(b)
 end
