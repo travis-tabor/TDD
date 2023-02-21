ab2ffb97024de258721b09557fbd162891ed6d00
Travis Tabor <123790499+travis-tabor@users.noreply.github.com>
2023-02-21 00:01:38 -0600

    Interval test issubset

--- test/interval_test.jl
+++ test/interval_test.jl
@@ -4,6 +4,7 @@ using Test
 a = Interval(1,5)
 b = Interval(5,3)
 c = Interval(1,3)
+d = Interval(3,6)
 
 @testset "interval.jl" begin
     @test typeof(a) == Interval
@@ -11,5 +12,7 @@ c = Interval(1,3)
     @test maximum(a) == 5
     @test in(2.5, a)
     @test isempty(b)
-    @test issubset(c,a)
+    @test issubset(c,a) == true
+    @test issubset(d,a) == false
+    @test issubset(b,a) == true
 end
