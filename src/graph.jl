61f16b2da78249824aa9169ecda8c9bb64f9d99f
Travis Tabor <123790499+travis-tabor@users.noreply.github.com>
2023-02-20 21:15:04 -0600

    Reorganized modules

---
+++ src/graph.jl
@@ -0,0 +1,12 @@
+module graph
+
+# Write your package code here.
+
+export direct_connect
+
+function direct_connect(input::Vector, node::Integer)
+    return sort(vcat(node, input[node]))
+end
+
+
+end
