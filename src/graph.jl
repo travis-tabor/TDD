136436c0762afcef89985cfd2fe174833ced2361
Travis Tabor <123790499+travis-tabor@users.noreply.github.com>
2023-02-21 07:12:00 -0600

    implement reachable

--- src/graph.jl
+++ src/graph.jl
@@ -1,5 +1,22 @@
-export direct_connect
+export direct_connect, reachable
 
 function direct_connect(input::Vector, node::Integer)
     return sort(vcat(node, input[node]))
 end
+
+function reachable(input::Vector, node::Integer)
+    visited = []
+    stack = [node]
+    while length(stack) > 0
+        n = pop!(stack)
+        if n ∉ visited
+            push!(visited,n)
+            stack = vcat(stack,direct_connect(input, n))
+        else 
+            continue
+        end
+    end
+    return sort(visited)
+end
+    
+     
