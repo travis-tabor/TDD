a228239f7192a9ce2c079194bb0b109fa34e1707
Travis Tabor <123790499+travis-tabor@users.noreply.github.com>
2023-02-20 19:06:48 -0600

    Correction

--- src/TDD.jl
+++ src/TDD.jl
@@ -6,8 +6,6 @@ export direct_connect
 
 function direct_connect(input::Vector, node::Integer)
     return sort(vcat(node, input[node]))
-
-function reachable(input::Vector, node::Integer)
-
+end
 
 end
